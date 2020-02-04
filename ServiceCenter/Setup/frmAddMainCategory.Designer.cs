4b60555d596e3249dd7f9d96a642b52d6ec8ff84
Nethsara Sadeepa Dilshan <nethsaradilshan97@gmail.com>
2020-02-04 11:54:21 +0530

    add main cat

---
+++ ServiceCenter/Setup/frmAddMainCategory.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace ServiceCenter.Setup
+{
+    partial class frmAddMainCategory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmAddMainCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Name = "frmAddMainCategory";
+            this.Text = "Add Main Category";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
