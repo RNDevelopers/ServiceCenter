8531841938e3c9ab10e7405bf8e6a08bc6f65787
Ramodk <42975445+Ramodk@users.noreply.github.com>
2020-02-04 17:32:41 +0530

    frm main

--- ServiceCenter/Setup/frmAddMainCategory.cs
+++ ServiceCenter/Setup/frmAddMainCategory.cs
@@ -15,7 +15,7 @@ namespace ServiceCenter.Setup
     {
         public frmAddMainCategory()
         {
-            InitializeComponent();
+            InitializeComponent();    
             this.SetFormName();  
         }
 
