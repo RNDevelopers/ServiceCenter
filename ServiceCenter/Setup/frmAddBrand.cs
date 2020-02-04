bc71d99f94e8aa85667610a142556391bd7f5bb2
Ramodk <42975445+Ramodk@users.noreply.github.com>
2020-02-04 21:28:06 +0530

    gdg

--- ServiceCenter/Setup/frmAddBrand.cs
+++ ServiceCenter/Setup/frmAddBrand.cs
@@ -1,8 +1,11 @@
 ﻿using ServiceCenter.Common;
+using ServiceCenter.DBConnection;
+using ServiceCenter.Enums;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,7 +20,71 @@ namespace ServiceCenter.Setup
         {
             InitializeComponent();
             this.SetFormName();
+            GridLoad();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveBrand();
+            Clear();
+            GridLoad();
+        }
+
+        public void Clear()
+        {
+            txtBrand.Text = string.Empty;
+        }
+
+        public void SaveBrand()
+        {
+            DialogResult dr = MessageBox.Show("Are You Sure Want to Add New Brand ?", "CONFIRM", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+
+            if (dr == DialogResult.Yes)
+            {
+
+                Execute objExecute = new Execute();
+                SqlParameter[] param = new SqlParameter[]
+                   {
+                    Execute.AddParameter("@vcName",txtBrand.Text.Trim().ToUpper()),
+                   };
+
+                int NoOfRowsEffected = objExecute.Executes("spSaveBrand", param, CommandType.StoredProcedure);
+
+                if (NoOfRowsEffected < 0)
+                {
+                    MessageBox.Show("Save..");
+                }
+                else
+                {
+                    MessageBox.Show("Cant't Save..");
+                }
+            }
+        }
+
+        public void GridLoad()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                Execute objExecute = new Execute();
+                string Query = "[dbo].[spGetBrand]";
+                SqlParameter[] para = new SqlParameter[]
+                  {
+   
+                  };
+
+                dt = (DataTable)objExecute.Executes(Query, ReturnType.DataTable, para, CommandType.StoredProcedure);
+
+                dgvBrand.DataSource = dt;
+            }
+
+            catch (Exception)
+            {
+
+                throw;
+            }
 
         }
+
     }
 }
