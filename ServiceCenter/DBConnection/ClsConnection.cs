a63c9aacb11225018be352964ae00c42949a4cce
Ramodk <42975445+Ramodk@users.noreply.github.com>
2020-02-04 15:41:48 +0530

    db

---
+++ ServiceCenter/DBConnection/ClsConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceCenter.DBConnection
+{
+    public class ClsConnection
+    {
+
+        private static string connetionString = ConfigurationManager.ConnectionStrings["MyDBConnectionString"].ConnectionString;
+
+    }
+}
