18ed5da2f90868c0160b44daf0b0a479cbb735c7
Ramodk <42975445+Ramodk@users.noreply.github.com>
2020-02-04 23:22:00 +0530

    AddSubCategory combo box and textbox working :)

--- ServiceCenter/Setup/frmAddSubCategory.cs
+++ ServiceCenter/Setup/frmAddSubCategory.cs
@@ -1,4 +1,6 @@
 ﻿using ServiceCenter.Common;
+using ServiceCenter.DBConnection;
+using ServiceCenter.Enums;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,11 +19,59 @@ namespace ServiceCenter.Setup
         {
             InitializeComponent();
             this.SetFormName();
+            GetCompany();
+            GetMainCategory();
+            GetBrand();
+            GetItemUnit();
         }
 
-        private void label10_Click(object sender, EventArgs e)
+        public void GetCompany()
         {
+            Execute objExecute = new Execute();
+            DataTable dt = (DataTable)objExecute.Executes("spGetAllCompany", ReturnType.DataTable, CommandType.StoredProcedure);
 
+            cmbCompanyName.DataSource = dt;
+            cmbCompanyName.DisplayMember = "vcCompanyName";
+            cmbCompanyName.ValueMember = "intCompanyID";
+            cmbCompanyName.SelectedIndex = -1;
+        }
+
+        public void GetMainCategory()
+        {
+            Execute objExecute = new Execute();
+            DataTable dt = (DataTable)objExecute.Executes("spGetMainCategory", ReturnType.DataTable, CommandType.StoredProcedure);
+
+            cmbMainCategory.DataSource = dt;
+            cmbMainCategory.DisplayMember = "vcMainCategoryName";
+            cmbMainCategory.ValueMember = "intMainCategoryID";
+            cmbMainCategory.SelectedIndex = -1;
+        }
+
+        public void GetBrand()
+        {
+            Execute objExecute = new Execute();
+            DataTable dt = (DataTable)objExecute.Executes("spGetBrand", ReturnType.DataTable, CommandType.StoredProcedure);
+
+            cmbBrand.DataSource = dt;
+            cmbBrand.DisplayMember = "vcName";
+            cmbBrand.ValueMember = "intBrandID";
+            cmbBrand.SelectedIndex = -1;
+        }
+
+        public void GetItemUnit()
+        {
+            Execute objExecute = new Execute();
+            DataTable dt = (DataTable)objExecute.Executes("spGetItemUnit", ReturnType.DataTable, CommandType.StoredProcedure);
+
+            cmbUnit.DataSource = dt;
+            cmbUnit.DisplayMember = "vcUnitDesc";
+            cmbUnit.ValueMember = "intUnitID";
+            cmbUnit.SelectedIndex = -1;
+        }
+
+        private void cmbBrand_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            txtSubCategory.Text = (cmbBrand.Text.ToString() + ' '+'-' + ' ');
         }
     }
 }
