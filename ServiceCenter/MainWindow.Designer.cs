1a4c7111bbc2a145e242b2fbdd7c3c7e9cfa6d42
Nethsara Sadeepa Dilshan <nethsaradilshan97@gmail.com>
2020-02-04 13:41:50 +0530

    fg

--- ServiceCenter/MainWindow.Designer.cs
+++ ServiceCenter/MainWindow.Designer.cs
@@ -32,36 +32,37 @@
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
-            // button1
+            // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(104, 68);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Location = new System.Drawing.Point(139, 84);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.Size = new System.Drawing.Size(132, 22);
             this.textBox3.TabIndex = 3;
             // 
             // checkedListBox1
             // 
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(599, 148);
-            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkedListBox1.Location = new System.Drawing.Point(680, 275);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(120, 72);
+            this.checkedListBox1.Size = new System.Drawing.Size(159, 72);
             this.checkedListBox1.TabIndex = 4;
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.textBox3);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.Name = "MainWindow";
             this.Text = "Form1";
             this.Controls.SetChildIndex(this.textBox3, 0);
             this.Controls.SetChildIndex(this.checkedListBox1, 0);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
