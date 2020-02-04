7f46f0bfb937419eb044982fc7c77f455e62ee76
Ramodk <42975445+Ramodk@users.noreply.github.com>
2020-02-04 11:52:29 +0530

    new add sup

--- ServiceCenter/MainWindow.Designer.cs
+++ ServiceCenter/MainWindow.Designer.cs
@@ -33,18 +33,19 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(216, 36);
+            this.textBox3.Location = new System.Drawing.Point(78, 55);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
             this.textBox3.TabIndex = 3;
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.textBox3);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainWindow";
             this.Text = "Form1";
             this.ResumeLayout(false);
