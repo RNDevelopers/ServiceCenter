0a890642846a940fff8d28e50eb485c7057c9bd8
Ramodk <42975445+Ramodk@users.noreply.github.com>
2020-02-05 20:49:21 +0530

    ramod

--- ServiceCenter/Setup/frmAddSubCategory.cs
+++ ServiceCenter/Setup/frmAddSubCategory.cs
@@ -16,6 +16,8 @@ namespace ServiceCenter.Setup
             GetMainCategory();
             GetBrand();
             GetItemUnit();
+
+            //from Ramod
         }
 
         public void GetCompany()
